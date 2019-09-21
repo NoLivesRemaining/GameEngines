39e2944e2dc895d787ae53e3aa6d638ab4c5bc60
NoLivesRemaining <31869017+NoLivesRemaining@users.noreply.github.com>
2019-09-21 18:25:33 +0100

    First Lab

---
+++ Game Engines/Assets/Scripts/MakeWall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeWall : MonoBehaviour
+{
+    public GameObject brick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Instantiate(brick, new Vector3(i + 0.25f, j + 0.25f, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
